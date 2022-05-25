ed2482cba73ac3647f1a2cda71cb917af6ef1b48
Vicix <lastapocalypse9@gmail.com>
2022-05-25 16:57:22 +0200

    Remove build files

--- builddir/subprojects/raylib/__CMake_build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Codes/c/router/subprojects/raylib
-# Build directory: D:/Codes/c/router/builddir/subprojects/raylib/__CMake_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
-subdirs("examples")
--- builddir/subprojects/raylib/__CMake_build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Codes/c/router/subprojects/raylib/src
-# Build directory: D:/Codes/c/router/builddir/subprojects/raylib/__CMake_build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
